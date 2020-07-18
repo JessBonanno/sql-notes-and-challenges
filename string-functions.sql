999a8d9cd2ba98ae0b36627fb533e44033784646
Jess <jessbonanno@gmail.com>
2020-07-18 17:42:17 -0400

    finihed substring challenges

--- string-functions.sql
+++ string-functions.sql
@@ -50,3 +50,60 @@ FROM books;
 
 -- * REPLACE()
 --  ? replaces parts of strings ie. all spaces with commas
+SELECT REPLACE('Hello World', 'Hell', '%$#@');
+
+SELECT REPLACE('Hello World', 'l', '7');
+
+SELECT REPLACE('Hello World', 'o', '0');
+
+SELECT REPLACE('HellO World', 'o', '*');
+
+SELECT
+    REPLACE('cheese bread coffee milk', ' ', ' and ');
+
+SELECT REPLACE(title, 'e ', '3')
+FROM books;
+
+SELECT
+    SUBSTRING(REPLACE(title, 'e', '3'), 1, 10)
+FROM books;
+
+SELECT
+    SUBSTRING(REPLACE(title, 'e', '3'), 1, 10) AS 'weird string'
+FROM books;
+
+-- * REVERSE()
+-- ? reverses strings
+SELECT REVERSE('Hello World');
+
+SELECT REVERSE('meow meow');
+
+SELECT REVERSE(author_fname)
+FROM books;
+
+SELECT CONCAT('woof', REVERSE('woof'));
+
+SELECT CONCAT(author_fname, REVERSE(author_fname))
+FROM books;
+
+--  * CHAR_LENGTH
+-- ? returns the number of characters in a string
+SELECT CHAR_LENGTH('Hello World');
+
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length'
+FROM books;
+
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long')
+FROM books;
+
+-- * UPPER() & LOWER()
+-- ? change case of a string making it all caps or all lowercase
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
